3638051047b3d7e4bb87f2840e321bd3233fd8f2
Gabriel Dominic K. Altea <altea.gabriel@yahoo.com>
2025-01-31 16:21:01 +0800

    update

--- script.js
+++ script.js
@@ -138,6 +138,35 @@ function showStory(index) {
     storyViewer.classList.add('active');
 }
 
+<<<<<<< Updated upstream
+=======
+
+function showNextStory(index) {
+    if (index + 1 < storyQueue.length) {
+        showStory(index + 1);
+    } else {
+        closeStoryViewer();
+    }
+}
+
+function closeStoryViewer() {
+    storyViewer.classList.remove('active');
+    clearTimeout(progressTimeout);
+
+    // Stop video if playing
+    const video = storyViewerContent.querySelector("video");
+    if (video) {
+        video.pause();
+        video.currentTime = 0; // Reset to the beginning
+    }
+
+    // Show footer again (only if it exists)
+    let footer = document.querySelector("footer");
+    if (footer) footer.style.display = "block";
+}
+
+
+>>>>>>> Stashed changes
 function updateProgressBar(duration, callback) {
     progressBar.style.width = '0%';
     progressBar.style.transition = 'none';
@@ -153,3 +182,105 @@ function updateProgressBar(duration, callback) {
         }, duration);
     });
 }
+<<<<<<< Updated upstream
+=======
+
+/*remove*/
+
+
+const stories = document.querySelector(".stories");
+const storiesFullView = document.querySelector(".stories-full-view");
+const closeBtn = document.querySelector(".close-btn");
+const storyImageFull = document.querySelector(".stories-full-view .story img");
+const storyAuthorFull = document.querySelector(
+  ".stories-full-view .story .author"
+);
+const nextBtn = document.querySelector(".stories-container .next-btn");
+const previousBtn = document.querySelector(".stories-container .previous-btn");
+const storiesContent = document.querySelector(".stories-container .content");
+const nextBtnFull = document.querySelector(".stories-full-view .next-btn");
+const previousBtnFull = document.querySelector(
+  ".stories-full-view .previous-btn"
+);
+
+let currentActive = 0;
+
+const createStories = () => {
+  allStories.forEach((s, i) => {
+    const story = document.createElement("div");
+    story.classList.add("story");
+    const img = document.createElement("img");
+    img.src = s.imageUrl;
+    const author = document.createElement("div");
+    author.classList.add("author");
+    author.innerHTML = s.author;
+
+    story.appendChild(img);
+    story.appendChild(author);
+
+    stories.appendChild(story);
+
+    story.addEventListener("click", () => {
+      showFullView(i);
+    });
+  });
+};
+
+createStories();
+
+const showFullView = (index) => {
+  currentActive = index;
+  updateFullView();
+  storiesFullView.classList.add("active");
+};
+
+closeBtn.addEventListener("click", () => {
+  storiesFullView.classList.remove("active");
+});
+
+const updateFullView = () => {
+  storyImageFull.src = allStories[currentActive].imageUrl;
+  storyAuthorFull.innerHTML = allStories[currentActive].author;
+};
+
+nextBtn.addEventListener("click", () => {
+  storiesContent.scrollLeft += 300;
+});
+
+previousBtn.addEventListener("click", () => {
+  storiesContent.scrollLeft -= 300;
+});
+
+storiesContent.addEventListener("scroll", () => {
+  if (storiesContent.scrollLeft <= 24) {
+    previousBtn.classList.remove("active");
+  } else {
+    previousBtn.classList.add("active");
+  }
+
+  let maxScrollValue =
+    storiesContent.scrollWidth - storiesContent.clientWidth - 24;
+
+  if (storiesContent.scrollLeft >= maxScrollValue) {
+    nextBtn.classList.remove("active");
+  } else {
+    nextBtn.classList.add("active");
+  }
+});
+
+nextBtnFull.addEventListener("click", () => {
+  if (currentActive >= allStories.length - 1) {
+    return;
+  }
+  currentActive++;
+  updateFullView();
+});
+
+previousBtnFull.addEventListener("click", () => {
+  if (currentActive <= 0) {
+    return;
+  }
+  currentActive--;
+  updateFullView();
+});
+>>>>>>> Stashed changes
